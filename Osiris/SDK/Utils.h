e9d06054986d8bf99412b26c3156eb2da0643eef
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2021-11-27 23:07:51 -0300

    Add ticksToTime

--- Osiris/SDK/Utils.h
+++ Osiris/SDK/Utils.h
@@ -8,5 +8,6 @@
 #include "../Memory.h"
 
 static auto timeToTicks(float time) noexcept { return static_cast<int>(0.5f + time / memory->globalVars->intervalPerTick); }
+static auto ticksToTime(int ticks) noexcept { return static_cast<float>(ticks * memory->globalVars->intervalPerTick); }
 
 std::tuple<float, float, float> rainbowColor(float speed) noexcept;

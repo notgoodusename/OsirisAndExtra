ca8ab21d27225cdf5783c90fbf197e095af8354b
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2021-12-22 16:22:28 -0300

    Complete GameMovement class

--- Osiris/SDK/GameMovement.h
+++ Osiris/SDK/GameMovement.h
@@ -9,4 +9,22 @@ class GameMovement {
 public:
     VIRTUAL_METHOD_V(void, processMovement, 1, (Entity* localPlayer, MoveData* moveData), (this, localPlayer, moveData))
     VIRTUAL_METHOD(Vector&, getPlayerViewOffset, 8, (bool ducked), (this, ducked))
+
+	PAD(4)
+	Entity* player;
+	MoveData* moveData;
+	int oldWaterLevel;
+	float waterEntryTime;
+	int onLadder;
+	Vector vecForward;
+	Vector vecRight;
+	Vector vecUp;
+	int cachedGetPointContents[64][3];
+	Vector cachedGetPointContentsPoint[64][3];
+	int speedCropped;
+	bool processingMovement;
+	bool inStuckTest;
+	float stuckCheckTime[64 + 1][2];
+	void* traceListData;
+	int traceCount;
 };

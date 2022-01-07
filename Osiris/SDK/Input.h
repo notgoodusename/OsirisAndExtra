d61cfc02c8bf6799df427fc17bb48e6d3ca61118
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2022-01-07 01:38:13 -0300

    Revert "Fix thirdperson"
    This reverts commit 61f6fba3823841586647513e66d8455ffe2f426a.

--- Osiris/SDK/Input.h
+++ Osiris/SDK/Input.h
@@ -5,13 +5,12 @@
 
 class Input {
 public:
-    VIRTUAL_METHOD(UserCmd*, getUserCmd, 8, (int slot, int sequenceNumber), (this, slot, sequenceNumber))
     PAD(12)
     bool isTrackIRAvailable;
     bool isMouseInitialized;
     bool isMouseActive;
     PAD(178)
     bool isCameraInThirdPerson;
-    PAD(1)
+    bool cameraMovingWithMouse;
     Vector cameraOffset;
 };

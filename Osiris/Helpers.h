72084b6e9b61bdfacc8545cc90adc7341ce482cc
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2022-03-27 15:16:07 -0300

    Change logConsole default color

--- Osiris/Helpers.h
+++ Osiris/Helpers.h
@@ -13,7 +13,7 @@ struct Vector;
 
 namespace Helpers
 {
-    void logConsole(std::string_view msg, const std::array<std::uint8_t, 4> color = { 0, 0, 0, 255 }) noexcept;
+    void logConsole(std::string_view msg, const std::array<std::uint8_t, 4> color = { 255, 255, 255, 255 }) noexcept;
     float simpleSpline(float value) noexcept;
     float simpleSplineRemapVal(float val, float A, float B, float C, float D) noexcept;
     float simpleSplineRemapValClamped(float val, float A, float B, float C, float D) noexcept;

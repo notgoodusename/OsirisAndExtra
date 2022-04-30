2835143355e983d57cd074a46605703e23d4ee9d
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2022-04-30 12:57:38 -0300

    Add processPacket hook
    Honestly idk what this does

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -65,6 +65,7 @@ public:
     VIRTUAL_METHOD(const Matrix4x4&, worldToScreenMatrix, 37, (), (this))
     VIRTUAL_METHOD(void*, getBSPTreeQuery, 43, (), (this))
     VIRTUAL_METHOD(const char*, getLevelName, 53, (), (this))
+    VIRTUAL_METHOD(void, fireEvents, 59, (), (this))
     VIRTUAL_METHOD(NetworkChannel*, getNetworkChannel, 78, (), (this))
     VIRTUAL_METHOD(bool, isHLTV, 93, (), (this))
     VIRTUAL_METHOD(void, clientCmdUnrestricted, 114, (const char* cmd, bool fromConsoleOrKeybind = false), (this, cmd, fromConsoleOrKeybind))

7f5aa395f274179b7f8632dc91be2255a7f23c65
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2022-07-24 00:21:49 -0300

    Add more variables to tickbase

--- Osiris/Hacks/Tickbase.cpp
+++ Osiris/Hacks/Tickbase.cpp
@@ -8,10 +8,17 @@
 #include "../SDK/UserCmd.h"
 
 int tickShift{ 0 };
+int shiftCommand{ 0 };
+int shiftedTickbase{ 0 };
 
 int Tickbase::getCorrectTickbase(int commandNumber) noexcept
 {
 	const int tickBase = localPlayer->tickBase();
+
+	if (commandNumber == shiftCommand)
+		return tickBase - shiftedTickbase;
+	else if (commandNumber == shiftCommand + 1)
+		return tickBase + shiftedTickbase;
 
 	return tickBase;
 }
@@ -23,5 +30,6 @@ int Tickbase::getTickshift() noexcept
 
 void Tickbase::resetTickshift() noexcept
 {
+	shiftedTickbase = tickShift;
 	tickShift = 0;
 }

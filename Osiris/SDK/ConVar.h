71c08bd8efa11dcc38b79df72c914fad21bb543f
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2022-10-30 23:12:33 -0300

    Update convar struct

--- Osiris/SDK/ConVar.h
+++ Osiris/SDK/ConVar.h
@@ -13,17 +13,11 @@ struct ConVar {
     VIRTUAL_METHOD(void, setValue, 15, (float value), (this, value))
     VIRTUAL_METHOD(void, setValue, 16, (int value), (this, value))
 
-    std::byte pad[24];
+    PAD(24)
     std::add_pointer_t<void __cdecl()> changeCallback;
     ConVar* parent;
     const char* defaultValue;
     char* string;
-    int stringLength;
-    float floatValue;
-    int intValue;
-    int hasMin;
-    float minValue;
-    int hasMax;
-    float maxValue;
+    PAD(44)
     UtlVector<void(__cdecl*)()> onChangeCallbacks;
 };

72a6389b77f13ff0f2cb401164c753dd776afc0f
xomarko <115811042+xomarko@users.noreply.github.com>
2022-11-06 12:51:46 +0800

    Update Visuals.cpp

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -880,8 +880,10 @@ void Visuals::skybox(FrameStage stage) noexcept
     if (stage != FrameStage::RENDER_START && stage != FrameStage::RENDER_END)
         return;
 
-    if (stage == FrameStage::RENDER_START && config->visuals.skybox > 0 && static_cast<std::size_t>(config->visuals.skybox) < skyboxList.size()) {
+    if (stage == FrameStage::RENDER_START && config->visuals.skybox > 0 && static_cast<std::size_t>(config->visuals.skybox) < skyboxList.size() - 1) {
         memory->loadSky(skyboxList[config->visuals.skybox]);
+    } else if (config->visuals.skybox == 26 && stage == FrameStage::RENDER_START) {
+        memory->loadSky(config->visuals.customSkybox.c_str());
     } else {
         static const auto sv_skyname = interfaces->cvar->findVar("sv_skyname");
         memory->loadSky(sv_skyname->string);

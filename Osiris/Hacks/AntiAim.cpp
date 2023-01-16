f616a8e265c3f1324e79daf9c15f0ad7e444bd79
渟雲 <QuQ@outlook.it>
2023-01-17 01:36:32 +0800

    forget else

--- Osiris/Hacks/AntiAim.cpp
+++ Osiris/Hacks/AntiAim.cpp
@@ -278,6 +278,8 @@ void AntiAim::rage(UserCmd* cmd, const Vector& previousViewAngles, const Vector&
             }
             if (config->rageAntiAim.roll && (config->rageAntiAim.rollAdd < 5 || !config->rageAntiAim.rollAdd || !(cmd->buttons & UserCmd::IN_JUMP || localPlayer->velocity().length2D() > 50.f)))
                 cmd->viewangles.z = invert ? config->rageAntiAim.rollAdd : config->rageAntiAim.rollAdd * -1.f;
+            else
+                cmd->viewangles.z = 0;
             if (sendPacket)
                 return;
 

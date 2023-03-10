f83e9ce31457abd2db55e3a71f197ed532ce4e7d
Marko <115811042+xomarko@users.noreply.github.com>
2023-03-10 21:11:37 +0800

    Update Config.h

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -318,6 +318,12 @@ public:
         Color4 molotovTimerBG{ 1.0f, 1.0f, 1.0f, 0.5f };
         Color4 molotovTimerTimer{ 0.0f, 0.0f, 1.0f, 1.0f };
         Color4 molotovTimerText{ 0.0f, 0.0f, 0.0f, 1.0f };
+         struct CustomPostProcessing {
+            bool enabled = false;
+            float worldExposure = 0.0f;
+            float modelAmbient = 0.0f;
+            float bloomScale = 0.0f;
+        } customPostProcessing;
     } visuals;
 
     std::array<item_setting, 36> skinChanger;

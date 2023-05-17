971db7f82261096838f3592a2ff8325ebcc27ad2
xs9 <61426269+Xsintashi@users.noreply.github.com>
2023-05-17 23:56:48 +0200

    Revert "Thirdperson Scope Transparency"
    This reverts commit 8ca37fbde7345d79ad3ade944b4989b0b6d5efd1.

--- Osiris/SDK/RenderView.h
+++ Osiris/SDK/RenderView.h
@@ -4,11 +4,10 @@
 
 class RenderView {
 public:
-
     VIRTUAL_METHOD(void, setBlend, 4, (float alpha), (this, alpha))
     VIRTUAL_METHOD(void, setColorModulation, 6, (const float* colors), (this, colors))
 
-    void setColorModulation(float r, float g, float b) noexcept
+    constexpr void setColorModulation(float r, float g, float b) noexcept
     {
         float color[3]{ r, g, b };
         setColorModulation(color);

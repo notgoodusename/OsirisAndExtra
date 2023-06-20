a852b3fd4b3fb6a4580fd06c0e781ecf84bee4b1
XJle6uIIIeK <82363409+XJle6uIIIeK@users.noreply.github.com>
2023-06-20 08:43:04 +0300

    Add files via upload

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -682,6 +682,7 @@ public:
     NETVAR(simulationTime, "CBaseEntity", "m_flSimulationTime", float)
     NETVAR(ownerEntity, "CBaseEntity", "m_hOwnerEntity", int)
     NETVAR(spotted, "CBaseEntity", "m_bSpotted", bool)
+    NETVAR(team, "CBaseEntity", "m_iTeamNum", Team)
 
     NETVAR(weapons, "CBaseCombatCharacter", "m_hMyWeapons", int[64])
     PNETVAR(wearables, "CBaseCombatCharacter", "m_hMyWearables", int)
